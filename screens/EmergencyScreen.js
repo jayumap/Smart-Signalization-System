2396391938fd3a1957cc8749c3418967cd8059cb
jayumap <umapjay4757@gmail.com>
2024-02-08 21:32:30 +0530

    directions screen added

--- screens/EmergencyScreen.js
+++ screens/EmergencyScreen.js
@@ -10,7 +10,7 @@ const EmergencyScreen = () => {
   return (
     <View>
       <Text>Emergency Screen</Text>
-      <Text>Ambulance Location: {}</Text>
+      <Text>Ambulance Location: {ambulanceLocation}</Text>
     </View>
   );
 };
