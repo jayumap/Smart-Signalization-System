63e4f96d7c1dba8629a33aa5cd05c8d5a531e8ce
jayumap <umapjay4757@gmail.com>
2024-02-22 19:37:26 +0530

    google signup method client added

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import AppNavigation from './navigation/appNavigation';
+// import { GoogleSignin } from '@react-native-google-signin/google-signin';
 
 // import { NavigationContainer } from '@react-navigation/native';
 // import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -8,7 +9,9 @@ import AppNavigation from './navigation/appNavigation';
 // import GameStore from './screens/gameStore';
 
 // const Tab = createBottomTabNavigator();
-
+// GoogleSignin.configure({
+//   webClientId: '686269354523-anlgu753btkia3jot1uuf51k4daiq3i1.apps.googleusercontent.com', 
+// });
 
 export default function App() {
   return (
