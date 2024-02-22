604f89af5ea9956262e9b7cf9264e7e38b26daff
jayumap <umapjay4757@gmail.com>
2024-02-22 19:56:17 +0530

    firebase store initiated

--- config/firebase.js
+++ config/firebase.js
@@ -4,6 +4,7 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth, initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { initializeFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -19,8 +20,12 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+const firestore = initializeFirestore(app);
+
 const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage),
 });
 
-export { auth };
+
+export { auth, firestore };
+// export { auth };
--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,3 +1,4 @@
+
 import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
 import { themeColors } from '../theme';
