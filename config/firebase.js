c70590351055a7628c8a94fcd5cab681daf471e9
jayumap <umapjay4757@gmail.com>
2024-02-24 18:47:46 +0530

    app.js error resolved

--- config/firebase.js
+++ config/firebase.js
@@ -4,7 +4,7 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth, initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
-import { initializeFirestore } from 'firebase/firestore';
+// import { initializeFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -20,12 +20,12 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const firestore = initializeFirestore(app);
+// const firestore = initializeFirestore(app);
 
 const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage),
 });
 
 
-export { auth, firestore };
-// export { auth };
+// export { auth, firestore };
+export { auth };
